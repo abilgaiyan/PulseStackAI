d2e18755ab804c2f42c9cf91e9fca9bf4a747be1
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-12 16:46:15 +0530

    fix(ms-008): correct provider resolver registration

--- src/PulseStack.Core/DependencyInjection/ProviderResolverRegistration.cs
+++ src/PulseStack.Core/DependencyInjection/ProviderResolverRegistration.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using PulseStack.Abstractions.Providers;
+using PulseStack.Core.Providers;
 
 namespace PulseStack.Core.DependencyInjection;
 
@@ -9,7 +10,7 @@ internal static class ProviderResolverRegistration
     public static IServiceCollection AddProviderResolver(
         this IServiceCollection services)
     {
-        services.TryAddSingleton<IProviderResolver, Providers.ProviderResolver>();
+        services.TryAddSingleton<IProviderResolver, ProviderResolver>();
         return services;
     }
 }
