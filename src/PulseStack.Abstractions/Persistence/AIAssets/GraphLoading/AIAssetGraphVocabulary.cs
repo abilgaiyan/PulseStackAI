4728b6ffbab5a0c26d7de9fecc9afed400d2f250
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-09-12 17:40:28 +0530

    MS-009.9B.1 add graph loading vocabulary

---
+++ src/PulseStack.Abstractions/Persistence/AIAssets/GraphLoading/AIAssetGraphVocabulary.cs
@@ -0,0 +1,34 @@
+namespace PulseStack.Abstractions.Persistence.AIAssets.GraphLoading;
+
+public enum AIAssetGraphRelationshipClass
+{
+    DistinguishedStructural,
+    InternalOwnership,
+    InternalMembership,
+    InternalDistribution,
+    DeclarativeReference,
+    ExplicitRequirement
+}
+
+public enum AIAssetGraphMaterializationAuthority
+{
+    Required,
+    Excluded
+}
+
+public enum AIAssetGraphBoundaryRole
+{
+    Structural,
+    Internal,
+    External,
+    NotApplicable
+}
+
+public static class AIAssetGraphDiagnosticCodes
+{
+    public const string RootDefinitionUnavailable = "AAG001";
+    public const string RequiredDefinitionUnavailable = "AAG002";
+    public const string ReferenceIdentityConflict = "AAG003";
+    public const string LineageIdentityConflict = "AAG004";
+    public const string RequiredMaterializationCycle = "AAG005";
+}
